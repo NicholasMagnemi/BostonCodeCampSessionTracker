fd7911202a9ea84e57ebdedf3fcbb1797b9d1080
Nicholas Magnemi <50030521+NicholasMagnemi@users.noreply.github.com>
2023-04-09 22:34:41 -0400

    Added Validation Class

---
+++ BostonCodeCampSessionTracker/Validation/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BostonCodeCampSessionTracker.Validation
+{
+    internal class Validator
+    {
+    }
+}
