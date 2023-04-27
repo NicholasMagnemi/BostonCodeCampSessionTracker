91d3888a8a21065052514a0484f57683878ef7e3
StoneJohnson7 <104584462+StoneJohnson7@users.noreply.github.com>
2023-04-27 13:01:15 -0400

    hhh

--- BostonCodeCampSessionTracker/SessionTrackerForm.Designer.cs
+++ BostonCodeCampSessionTracker/SessionTrackerForm.Designer.cs
@@ -104,29 +104,29 @@
             lblFirstName = new Label();
             lblSpeakersForm = new Label();
             tbcSessionTracker = new TabControl();
-            tabAttendance = new TabPage();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            button1 = new Button();
             tabPassword = new TabPage();
-            label6 = new Label();
             txtPassword = new TextBox();
+            lblPassword = new Label();
+            tabAttendance = new TabPage();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             tbOverview.SuspendLayout();
             tbSessionInfo.SuspendLayout();
             tbTimeSlotInfo.SuspendLayout();
             tbRoomInfo.SuspendLayout();
             tbSpeakerInfo.SuspendLayout();
             tbcSessionTracker.SuspendLayout();
-            tabAttendance.SuspendLayout();
             tabPassword.SuspendLayout();
+            tabAttendance.SuspendLayout();
             SuspendLayout();
             // 
             // tbOverview
@@ -606,7 +606,7 @@
             tbRoomInfo.Controls.Add(label11);
             tbRoomInfo.Location = new Point(4, 29);
             tbRoomInfo.Name = "tbRoomInfo";
-            tbRoomInfo.Padding = new Padding(3, 3, 3, 3);
+            tbRoomInfo.Padding = new Padding(3);
             tbRoomInfo.Size = new Size(581, 498);
             tbRoomInfo.TabIndex = 1;
             tbRoomInfo.Text = "Room Info";
@@ -739,7 +739,7 @@
             tbSpeakerInfo.Controls.Add(lblSpeakersForm);
             tbSpeakerInfo.Location = new Point(4, 29);
             tbSpeakerInfo.Name = "tbSpeakerInfo";
-            tbSpeakerInfo.Padding = new Padding(3, 3, 3, 3);
+            tbSpeakerInfo.Padding = new Padding(3);
             tbSpeakerInfo.Size = new Size(581, 498);
             tbSpeakerInfo.TabIndex = 0;
             tbSpeakerInfo.Text = "Speaker Info";
@@ -969,9 +969,38 @@
             tbcSessionTracker.Location = new Point(1, 0);
             tbcSessionTracker.Name = "tbcSessionTracker";
             tbcSessionTracker.SelectedIndex = 0;
-            tbcSessionTracker.Size = new Size(589, 531);
+            tbcSessionTracker.Size = new Size(644, 531);
             tbcSessionTracker.TabIndex = 2;
             tbcSessionTracker.Click += tbcSessionTracker_Click;
+            // 
+            // tabPassword
+            // 
+            tabPassword.Controls.Add(txtPassword);
+            tabPassword.Controls.Add(lblPassword);
+            tabPassword.Location = new Point(4, 29);
+            tabPassword.Name = "tabPassword";
+            tabPassword.Size = new Size(636, 498);
+            tabPassword.TabIndex = 6;
+            tabPassword.Text = "Password";
+            tabPassword.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(189, 242);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(237, 27);
+            txtPassword.TabIndex = 1;
+            txtPassword.TextChanged += textBox6_TextChanged;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPassword.Location = new Point(31, 231);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(138, 38);
+            lblPassword.TabIndex = 0;
+            lblPassword.Text = "Password:";
             // 
             // tabAttendance
             // 
@@ -993,45 +1022,50 @@
             tabAttendance.Text = "Attendance";
             tabAttendance.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // button1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(36, 54);
-            label1.Name = "label1";
-            label1.Size = new Size(81, 28);
-            label1.TabIndex = 0;
-            label1.Text = "Session:";
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(443, 434);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 37);
+            button1.TabIndex = 10;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // textBox5
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(36, 333);
-            label2.Name = "label2";
-            label2.Size = new Size(107, 28);
-            label2.TabIndex = 1;
-            label2.Text = "End Count:";
+            textBox5.Location = new Point(36, 164);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 9;
             // 
-            // label3
+            // textBox4
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(36, 268);
-            label3.Name = "label3";
-            label3.Size = new Size(136, 28);
-            label3.TabIndex = 2;
-            label3.Text = "Middle Count:";
+            textBox4.Location = new Point(36, 236);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 8;
             // 
-            // label4
+            // textBox3
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(36, 194);
-            label4.Name = "label4";
-            label4.Size = new Size(162, 28);
-            label4.TabIndex = 3;
-            label4.Text = "Beginning Count:";
+            textBox3.Location = new Point(36, 303);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(36, 379);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(36, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 5;
             // 
             // label5
             // 
@@ -1043,85 +1077,51 @@
             label5.TabIndex = 4;
             label5.Text = "Room:";
             // 
-            // textBox1
+            // label4
             // 
-            textBox1.Location = new Point(36, 95);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 5;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(36, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(162, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Beginning Count:";
             // 
-            // textBox2
+            // label3
             // 
-            textBox2.Location = new Point(36, 379);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 6;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(36, 268);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Middle Count:";
             // 
-            // textBox3
+            // label2
             // 
-            textBox3.Location = new Point(36, 303);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 7;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(36, 333);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 28);
+            label2.TabIndex = 1;
+            label2.Text = "End Count:";
             // 
-            // textBox4
+            // label1
             // 
-            textBox4.Location = new Point(36, 236);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
-            textBox4.TabIndex = 8;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(36, 164);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
-            textBox5.TabIndex = 9;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(443, 434);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 37);
-            button1.TabIndex = 10;
-            button1.Text = "Save";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // tabPassword
-            // 
-            tabPassword.Controls.Add(txtPassword);
-            tabPassword.Controls.Add(label6);
-            tabPassword.Location = new Point(4, 29);
-            tabPassword.Name = "tabPassword";
-            tabPassword.Size = new Size(581, 498);
-            tabPassword.TabIndex = 6;
-            tabPassword.Text = "Password";
-            tabPassword.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label6.Location = new Point(31, 231);
-            label6.Name = "label6";
-            label6.Size = new Size(138, 38);
-            label6.TabIndex = 0;
-            label6.Text = "Password:";
-            // 
-            // txtPassword
-            // 
-            txtPassword.Location = new Point(189, 242);
-            txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(237, 27);
-            txtPassword.TabIndex = 1;
-            txtPassword.TextChanged += textBox6_TextChanged;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(36, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Session:";
             // 
             // SessionTrackerForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(589, 528);
+            ClientSize = new Size(608, 528);
             Controls.Add(tbcSessionTracker);
             Margin = new Padding(3, 4, 3, 4);
             Name = "SessionTrackerForm";
@@ -1137,10 +1137,10 @@
             tbSpeakerInfo.ResumeLayout(false);
             tbSpeakerInfo.PerformLayout();
             tbcSessionTracker.ResumeLayout(false);
-            tabAttendance.ResumeLayout(false);
-            tabAttendance.PerformLayout();
             tabPassword.ResumeLayout(false);
             tabPassword.PerformLayout();
+            tabAttendance.ResumeLayout(false);
+            tabAttendance.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -1225,7 +1225,7 @@
         private Label lblOverviewTimeDuration;
         private TabPage tabPassword;
         private TextBox txtPassword;
-        private Label label6;
+        private Label lblPassword;
         private TabPage tabAttendance;
         private Button button1;
         private TextBox textBox5;
